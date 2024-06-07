46e74b86779ef0b3c0618238797ab0ccbb6631ad
Durga Prasad Bejawada <117895509+Durga0808@users.noreply.github.com>
2024-06-07 12:47:49 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 //your JS code here. If required.
+
+function getFormvalue() {
+	
+	let form=document.getElementById('form1');
+	let fname=form.elements["First Name"].value;
+	let lname=form.elements["Last Name"].value;
+	let ph=form.elements["Phone Number"].value;
+	let email=form.elements["Email ID"].value;
+	let ans="First Name: "+fname+" Last Name: "+lname+" Phone Number: "+ph+" Email ID: "+email;
+	alert(ans);
+}
